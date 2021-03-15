f72f319cec62867b034fb13b000eef4b1974c1cc
Lcoderfit <1297611505@qq.com>
2021-03-15 20:36:17 +0800

    feature:添加文件操作使用工具

--- file/README.md
+++ file/README.md
@@ -2,3 +2,13 @@
 e: fork/exec grep -q "d" /home/learnGoroutine/file/src.log: no such file or directory
 s: exec.Command中的参数不能带空格，如果命令本身有空格，则需要根据空格分拆成多个参数传入
 cmd := exec.Command("grep", "-q", "d", "/home/learnGoroutine/file/src.log&&", "echo", "contain", "||", "not contain")
+
+2.长命令无效
+s:使用 bash -c `command string`, command string 可以表示一整条命令
+		cmd := exec.Command(
+			"bash", "-c",
+			fmt.Sprintf(`grep -q %s_ds %s && echo %d || echo %d`, key, path, GrepContain, GrepNotContain),
+		)
+3.
+注：Go语言中字典是无序的
+sort.Strings()可以对字符串列表进行排序：
--- file/file_test.go
+++ file/file_test.go
@@ -2,8 +2,110 @@ package file
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 )
+
+var source = map[string]map[string]string{
+	"mini": {
+		"shareholder":            "shareholder",
+		"KeyPerson":              "KeyPerson",
+		"company_base_info":      "entbaseInfo",
+		"branch":                 "branch",
+		"alter":                  "alter",
+		"spotCheckInfo":          "spotCheckInfo",
+		"licenceinfoDetail":      "licenceinfoDetail",
+		"insLicenceinfo":         "insLicenceinfo",
+		"punishmentdetail":       "punishmentdetail",
+		"illInfo":                "illInfo",
+		"liquidation":            "liquidation",
+		"insinvinfo":             "insinvinfo",
+		"insAlterstockinfo":      "insAlterstockinfo",
+		"entBusExcep":            "entBusExcep",
+		"stakqualitinfo":         "stakqualitinfo",
+		"mortreginfo":            "mortreginfo",
+		"assistInfo":             "assistInfo",
+		"insProPledgeRegInfo":    "insProPledgeRegInfo",
+		"report_base":            "report_base",
+		"report_webinfo":         "report_webinfo",
+		"report_investor":        "report_investor",
+		"report_investment":      "report_investment",
+		"report_change":          "report_change",
+		"report_social_security": "report_social_security",
+		"report_guarantee":       "report_guarantee",
+		"report_alter_stockinfo": "report_alter_stockinfo",
+	},
+	"pc": {
+		"shareholder":            "shareholder",
+		"KeyPerson":              "KeyPerson",
+		"company_base_info":      "base",
+		"shareholderDetail":      "shareholderDetail",
+		"branch":                 "branch",
+		"alter":                  "alter",
+		"spotCheckInfo":          "spotCheckInfo",
+		"licenceinfoDetail":      "licenceinfoDetail",
+		"insLicenceinfo":         "insLicenceinfo",
+		"punishmentdetail":       "punishmentdetail",
+		"illInfo":                "illInfo",
+		"liquidation":            "liquidation",
+		"insInvinfo":             "insInvinfo",
+		"insAlterstockinfo":      "insAlterstockinfo",
+		"entBusExcep":            "entBusExcep",
+		"stakqualitinfo":         "stakqualitinfo",
+		"mortreginfo":            "mortreginfo",
+		"mortregpersoninfo":      "mortregpersoninfo",
+		"mortCreditorRightInfo":  "mortCreditorRightInfo",
+		"mortGuaranteeInfo":      "mortGuaranteeInfo",
+		"getMortAltItemInfo":     "getMortAltItemInfo",
+		"getMortRegCancelInfo":   "getMortRegCancelInfo",
+		"assistInfo":             "assistInfo",
+		"assistInfoDetail":       "assistInfoDetail",
+		"insProPledgeRegInfo":    "insProPledgeRegInfo",
+		"report_base":            "report_base",
+		"report_webinfo":         "report_webinfo",
+		"report_investor":        "report_investor",
+		"report_investment":      "report_investment",
+		"report_change":          "report_change",
+		"report_social_security": "report_social_security",
+		"report_guarantee":       "report_guarantee",
+		"report_alter_stockinfo": "report_alter_stockinfo",
+	},
+	"diqu": {
+		"shareholder":            "shareholder",
+		"KeyPerson":              "KeyPerson",
+		"company_base_info":      "base",
+		"shareholderDetail":      "shareholderDetail",
+		"branch":                 "branch",
+		"alter":                  "alter",
+		"spotCheckInfo":          "spotCheckInfo",
+		"licenceinfoDetail":      "licenceinfoDetail",
+		"insLicenceinfo":         "insLicenceinfo",
+		"punishmentdetail":       "punishmentdetail",
+		"illInfo":                "illInfo",
+		"liquidation":            "liquidation",
+		"insInvinfo":             "insInvinfo",
+		"insAlterstockinfo":      "insAlterstockinfo",
+		"entBusExcep":            "entBusExcep",
+		"stakqualitinfo":         "stakqualitinfo",
+		"mortreginfo":            "mortreginfo",
+		"mortregpersoninfo":      "mortregpersoninfo",
+		"mortCreditorRightInfo":  "mortCreditorRightInfo",
+		"mortGuaranteeInfo":      "mortGuaranteeInfo",
+		"getMortAltItemInfo":     "getMortAltItemInfo",
+		"getMortRegCancelInfo":   "getMortRegCancelInfo",
+		"assistInfo":             "assistInfo",
+		"assistInfoDetail":       "assistInfoDetail",
+		"insProPledgeRegInfo":    "insProPledgeRegInfo",
+		"report_base":            "report_base",
+		"report_webinfo":         "report_webinfo",
+		"report_investor":        "report_investor",
+		"report_investment":      "report_investment",
+		"report_change":          "report_change",
+		"report_social_security": "report_social_security",
+		"report_guarantee":       "report_guarantee",
+		"report_alter_stockinfo": "report_alter_stockinfo",
+	},
+}
 
 func TestFileExist(t *testing.T) {
 	path := "./a.txt"
@@ -21,17 +123,34 @@ func TestFileExist(t *testing.T) {
 }
 
 func TestFindNotExistKeys(t *testing.T) {
-	path := "src.log"
-	keys := []string{
-		"d",
-		"hello",
+	//path := "/home/learnGoroutine/file/src.log"
+	path := "/home/learnGoroutine/file/gongshang_pc_test.log"
+	var keys []string
+	for _, v := range source["pc"] {
+		keys = append(keys, v)
 	}
+	//sort.Strings(keys)
 	output, err := FindNotExistKeys(path, keys)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("h1")
+	// 对字符串列表进行排序, 从而保证输出结果总是一样的
+	sort.Strings(output)
 	fmt.Println(output)
-	fmt.Println("h1")
+}
+
+func BenchmarkFindNotExistKeys(b *testing.B) {
+	path := "/home/learnGoroutine/file/gongshang_pc_test.log"
+	var keys []string
+	for _, v := range source["pc"] {
+		keys = append(keys, v)
+	}
+	for i := 0; i < b.N; i++ {
+		_, err := FindNotExistKeys(path, keys)
+		if err != nil {
+			b.Error(err)
+			return
+		}
+	}
 }
