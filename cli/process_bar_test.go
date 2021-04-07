1fe5c4470c6536b4854890a664069c1f3bef85ee
Lcoderfit <1297611505@qq.com>
2021-04-07 09:28:47 +0800

    feature:添加进度条实用工具

---
+++ cli/process_bar_test.go
@@ -0,0 +1,16 @@
+package cli
+
+import (
+	"testing"
+	"time"
+)
+
+func TestProcessBar(t *testing.T) {
+	var bar Bar
+	bar.NewOption(0, 100)
+	for i := 0; i <= 100; i++ {
+		time.Sleep(100 * time.Millisecond)
+		bar.Play(int64(i))
+	}
+	bar.Finish()
+}
